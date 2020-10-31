daa7c8d30bbc9a7bb2bdbeef289d10dfac7ef714
Steve Cox <scox@renci.org>
2020-10-31 15:08:02 -0400

    works in airflow (alpha)

--- tranql_translate.py
+++ tranql_translate.py
@@ -16,10 +16,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
 """
-This is an example dag for using a Kubernetes Executor Configuration.
+An Airflow workflow for the Roger Translator KGX data pipeline.
 """
-from __future__ import print_function
+#from __future__ import print_function
 
 import os
 import subprocess
@@ -28,11 +29,11 @@ from airflow.contrib.example_dags.libs.helper import print_stuff
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
-from roger.core import RogerUtil #KGXModel, BiolinkModel, BulkLoad
+from roger.core import RogerUtil
 
 default_args = {
     'owner': 'RENCI',
-    'start_date': days_ago(2)
+    'start_date': days_ago(1)
 }
 
 with DAG(
@@ -40,41 +41,32 @@ with DAG(
     default_args=default_args,
     schedule_interval=None
 ) as dag:
-        
-    intro = BashOperator(
-        task_id='Intro',
-        bash_command='echo running tranql translator'
-    )
-    get_t = PythonOperator(
-        task_id="GetSource",
-        op_kwargs={ 'to_string' : True },
-        python_callable=RogerUtil.get_kgx
-    )
-    schema_t = PythonOperator(
-        task_id="InferSchema",
-        op_kwargs={ 'to_string' : True },
-        python_callable=RogerUtil.create_schema
-    )
-    merge_t = PythonOperator(
-        task_id="MergeNodes",
-        op_kwargs={ 'to_string' : True },
-        python_callable=RogerUtil.merge_nodes
-    )
-    bulk_create_t = PythonOperator(
-        task_id="CreateBulkLoad",
-        op_kwargs={ 'to_string' : True },
-        python_callable=RogerUtil.create_bulk_load
-    )
-    finish = BashOperator(
-        task_id='Finish',
-        bash_command='echo finish'
-    )
-    intro >> get_t >> [ schema_t, merge_t ] >> bulk_create_t >> finish
+    
+    at_k8s=False
+    
+    def get_executor_config (annotations=None):
+        k8s_executor_config={
+            "KubernetesExecutor": {
+                "annotations": annotations
+            }
+        }
+        return k8s_executor_config if at_k8s else None
 
-    """,
-    executor_config={
-    "KubernetesExecutor": {
-    "annotations": {
-    "test" : "annotation"
-    }}}
-    """
+    def create_python_task (name, a_callable):
+        return PythonOperator(
+            task_id=name,
+            python_callable=a_callable,
+            op_kwargs={ 'to_string' : True },
+            executor_config=get_executor_config (annotations={
+                "task_name" : name
+            }))
+
+    intro = BashOperator(task_id='Intro', bash_command='echo running tranql translator')
+    get_kgx = create_python_task ("GetSource", RogerUtil.get_kgx)
+    create_schema = create_python_task ("CreateSchema", RogerUtil.create_schema)
+    merge_nodes = create_python_task ("MergeNodes", RogerUtil.merge_nodes)
+    create_bulk_load = create_python_task ("CreateBulkLoad", RogerUtil.create_bulk_load)
+    finish = BashOperator (task_id='Finish', bash_command='echo finish')
+    
+    intro >> get_kgx >> [ create_schema, merge_nodes ] >> create_bulk_load >> finish
+    
