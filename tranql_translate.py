bf4bff1e6b77590891c56cb3c31e07ecf4a11a28
Steve Cox <scox@renci.org>
2020-10-31 15:28:54 -0400

    works in airflow (alpha)

--- tranql_translate.py
+++ tranql_translate.py
@@ -1,26 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
 
 """
 An Airflow workflow for the Roger Translator KGX data pipeline.
 """
-#from __future__ import print_function
 
 import os
 import subprocess
@@ -36,16 +19,22 @@ default_args = {
     'start_date': days_ago(1)
 }
 
+""" Build the workflow's tasks and DAG. """
 with DAG(
     dag_id='tranql_translate',
     default_args=default_args,
     schedule_interval=None
 ) as dag:
-    
+
+    """ Configure use of KubernetesExecutor. """
     at_k8s=False
     
     def get_executor_config (annotations=None):
-        k8s_executor_config={
+        """ Get an executor configuration.
+        :param annotations: Annotations to attach to the executor.
+        :returns: Returns a KubernetesExecutor if K8s is configured and None otherwise.
+        """
+        k8s_executor_config = {
             "KubernetesExecutor": {
                 "annotations": annotations
             }
@@ -53,20 +42,27 @@ with DAG(
         return k8s_executor_config if at_k8s else None
 
     def create_python_task (name, a_callable):
+        """ Create a python task.
+        :param name: The name of the task.
+        :param a_callable: The code to run in this task.
+        """
         return PythonOperator(
             task_id=name,
             python_callable=a_callable,
             op_kwargs={ 'to_string' : True },
             executor_config=get_executor_config (annotations={
                 "task_name" : name
-            }))
+            })
+        )
 
+    """ Build the workflow tasks. """
     intro = BashOperator(task_id='Intro', bash_command='echo running tranql translator')
     get_kgx = create_python_task ("GetSource", RogerUtil.get_kgx)
     create_schema = create_python_task ("CreateSchema", RogerUtil.create_schema)
     merge_nodes = create_python_task ("MergeNodes", RogerUtil.merge_nodes)
     create_bulk_load = create_python_task ("CreateBulkLoad", RogerUtil.create_bulk_load)
     finish = BashOperator (task_id='Finish', bash_command='echo finish')
-    
+
+    """ Build the DAG. """
     intro >> get_kgx >> [ create_schema, merge_nodes ] >> create_bulk_load >> finish
     
