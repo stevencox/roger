ff058fc1f5c8f51da4f59d35172ed9a49eb0d88b
YaphetKG <kebedey@renci.org>
2021-01-04 10:52:41 -0500

    adds type casting

---
+++ roger/components/data_conversion_utils.py
@@ -0,0 +1,54 @@
+from typing import Any
+
+
+class TypeConversionUtil:
+
+    type_map = {
+        list.__name__: {
+            'priority': 0,
+            'constructor': lambda x: list([x])
+        },
+        str.__name__: {
+            'priority': 1,
+            'constructor': lambda x: str(x)
+        },
+        bool.__name__: {
+            'priority': 2,
+            'constructor': lambda x: True if x else False
+        },
+        float.__name__: {
+            'priority': 3,
+            'constructor': lambda x: float(x),
+        },
+        int.__name__: {
+            'priority': 4,
+            'constructor': lambda x: int(x)
+        }
+    }
+
+    @staticmethod
+    def cast(value: Any, to_type: str):
+        """
+        Parses a value to dest type.
+        :param value: value to parse
+        :param to_type: destination type
+        :return: parsed value
+        """
+        if to_type not in TypeConversionUtil.type_map:
+            raise TypeError(f'Type {to_type} not found in conversion map. Available types are {TypeConversionUtil.type_map.keys()}')
+        dest_type_constructor = TypeConversionUtil.type_map[to_type]['constructor']
+        return dest_type_constructor(value)
+
+    @staticmethod
+    def compare_types(data_type: str, data_type_2: str):
+        """
+        Of two python types selects the one we would like to upcast to.
+        :param data_type:
+        :param data_type_2:
+        :return:
+        """
+        assert data_type in TypeConversionUtil.type_map, f"Unrecognised type {data_type} for list of types {list(type_map.keys())}"
+        assert data_type_2 in TypeConversionUtil.type_map, f"Unrecognised type {data_type} for list of types {list(type_map.keys())}"
+        d1_val = TypeConversionUtil.type_map[data_type]['priority']
+        d2_val = TypeConversionUtil.type_map[data_type_2]['priority']
+        return data_type if d1_val < d2_val else data_type_2
