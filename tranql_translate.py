2286a45fe730ceb25e9f5d3e402202eff47f6ec5
YaphetKG <kebedey@renci.org>
2021-01-12 08:55:59 -0500

    changing image for test

--- tranql_translate.py
+++ tranql_translate.py
@@ -39,7 +39,7 @@ with DAG(
         k8s_executor_config = {
             "KubernetesExecutor": {
                 "annotations": annotations,
-                "image": "renciorg/airflow-roger:latest"
+                "image": "renciorg/roger-executor:latest"
             }
         }
         return k8s_executor_config if at_k8s else None
