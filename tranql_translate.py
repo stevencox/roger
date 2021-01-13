437c76672d4e5abdf375c749294150c87dd339bf
YaphetKG <kebedey@renci.org>
2021-01-13 08:42:37 -0500

    data volume mount pod override.

--- tranql_translate.py
+++ tranql_translate.py
@@ -8,7 +8,6 @@ An Airflow workflow for the Roger Translator KGX data pipeline.
 import os
 import subprocess
 from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.example_dags.libs.helper import print_stuff
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
@@ -30,16 +29,28 @@ with DAG(
 
     """ Configure use of KubernetesExecutor. """
     at_k8s=True
-    
-    def get_executor_config (annotations=None):
+
+    def get_executor_config (annotations=None, data_path='/opt/roger/data'):
         """ Get an executor configuration.
         :param annotations: Annotations to attach to the executor.
         :returns: Returns a KubernetesExecutor if K8s is configured and None otherwise.
         """
         k8s_executor_config = {
             "KubernetesExecutor": {
-                "annotations": annotations,
-                "image": "renciorg/roger-executor:latest"
+                "volumes": [
+                    {
+                        "name": "roger-data",
+                        "persistentVolumeClaim": {
+                            "claimName": "roger-data-pvc"
+                        }
+                    }
+                ],
+                "volume_mounts": [
+                    {
+                        "mountPath": data_path,
+                        "name": "roger-data",
+                    }
+                ]
             }
         }
         return k8s_executor_config if at_k8s else None
@@ -56,6 +67,7 @@ with DAG(
         dag_conf = {}
         logger = get_logger()
         config = get_config()
+        config.update({'data_root': kwargs.get('data_path')})
         if dag_run:
             dag_conf = dag_run.conf
             # remove this since to send every other argument to the python callable.
@@ -71,22 +83,26 @@ with DAG(
         :param name: The name of the task.
         :param a_callable: The code to run in this task.
         """
+        data_path = '/opt/roger/data'
         return PythonOperator(
             task_id=name,
             python_callable=task_wrapper,
             op_kwargs={
                 "python_callable": a_callable,
-                "to_string": True
+                "to_string": True,
+                "data_path": data_path,
             },
             executor_config=get_executor_config (annotations={
-                "task_name" : name
-            }),
+                "task_name": name
+            }, data_path=data_path),
             dag=dag,
             provide_context=True
         )
 
     """ Build the workflow tasks. """
-    intro = BashOperator(task_id='Intro', bash_command='echo running tranql translator && exit 0')
+    intro = BashOperator(task_id='Intro',
+                         bash_command='echo running tranql translator && exit 0',
+                         executor_config= get_executor_config())
     get_kgx = create_python_task ("GetSource", RogerUtil.get_kgx)
     create_schema = create_python_task ("CreateSchema", RogerUtil.create_schema)
     merge_nodes = create_python_task ("MergeNodes", RogerUtil.merge_nodes)
