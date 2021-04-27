23d6e2612c6c29a4b33abf5f5caad7188ea7a7b4
Carl Schreep <schreepc@renci.org>
2021-04-27 08:39:34 -0400

    Adding ability to configure data output dir via env var

--- dags/roger/__init__.py
+++ dags/roger/__init__.py
@@ -1,3 +1,11 @@
+import os
 from pathlib import Path
 
-ROGER_DATA_DIR = Path(__file__).parent.resolve() / 'data'
+data_dir_env_value = os.getenv("ROGER_DATA_DIR")
+
+if data_dir_env_value is None:
+    ROGER_DATA_DIR = Path(__file__).parent.resolve() / 'data'
+else:
+    ROGER_DATA_DIR = Path(data_dir_env_value)
+
+
